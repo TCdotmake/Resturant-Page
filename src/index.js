3f0ed063528ce3c747f9480242d463d95024e662
TCdotmake <tommyjchen@hotmail.com>
2022-10-02 20:17:47 -0400

    created logo svg and placed it on page successfully

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 //imports
 import './style.css';
-
+import Logo from '../images/stackLogo.svg';
 //main content
 const content = document.createElement('div');
 content.id = 'content';
 content.textContent = 'Something else';
 document.body.insertAdjacentElement("beforeend", content);
+
+//try to put in logo
+const logo = document.createElement('div');
+logo.classList.add('logo');
+content.insertAdjacentElement('beforeend', logo);
